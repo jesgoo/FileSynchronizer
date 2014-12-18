5470f5dd5b615cdb9a046e528301295793e9a161
杨晨醒 <chenxing.yang@gmail.com>
2014-12-18 16:55:07 +0800

    简化代码

--- bin/directory_monitor.py
+++ bin/directory_monitor.py
@@ -12,34 +12,10 @@ import jesgoo.application
 import jesgoo.supervisorutil.log as log
 
 
-class ServerGroup(object):
-    _GROUPS = collections.defaultdict(list)
-
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def group(name):
-        return ServerGroup._GROUPS[name]
-
-    @staticmethod
-    def localhost_group():
-        group = ServerGroup.group('localhost')
-        if not group:
-            group.extend([
-                'localhost',
-                '127.0.0.1'
-            ])
-        return group
-
-    @staticmethod
-    def is_localhost(server):
-        return server in ('127.0.0.1', 'localhost') or server in ServerGroup.localhost_group()
+server_groups = collections.defaultdict(list)
 
 
 class DirectoryMonitor(watchdog.events.FileSystemEventHandler):
-    PushConfig = collections.namedtuple('PushConfig', ('local_path', 'remote_path', 'server_group'))
-
     def __init__(self, path, files_synchronizing=None, directory_synchronizing=None,
                  file_synchorinzing_timeout=5, directory_synchronizing_timeout=60):
         self._path = os.path.abspath(path)
@@ -76,70 +52,35 @@ class DirectoryMonitor(watchdog.events.FileSystemEventHandler):
                 self.synchronize_file(path, file_synchronizing_config.remote_path,
                                       file_synchronizing_config.server_group)
             for directory_synchronizing_config in self._directory_synchronizing_configs:
-                self.synchronize_directory(directory_synchronizing_config)
+                self.synchronize_file(self._path, directory_synchronizing_config.remote_path,
+                                      directory_synchronizing_config.server_group)
 
     def synchronize_file(self, local_path, remote_path, server_group):
         log.debug('同步单文件: %s', local_path)
-        processes = [(server,
-                      subprocess.Popen(['scp', local_path, '%s:%s' % (server, remote_path)])
-                      if not ServerGroup.is_localhost(server) else
-                      subprocess.Popen(['cp', local_path, remote_path]))
-                     for server in ServerGroup.group(server_group)]
-        running_processes = processes
+        processes = [(server, subprocess.Popen(['rsync', '-r', '-T', '/tmp',
+                                                local_path, '%s:%s' % (server, remote_path)]))
+                     for server in server_groups[server_group]]
+        running_process = processes
         deadline = time.time() + self._files_synchronizing_timeout
-        while 1:
-            if time.time() >= deadline:
-                break
-            processes = running_processes
-            running_processes = []
-            for server, process in processes:
-                return_code = process.poll()
-                if return_code is None:
-                    running_processes.append((server, process))
-                elif return_code != 0:
-                    log.error('同步文件失败: local_path=%s, server=%s, remote_path=%s, return_code=%d',
-                              local_path, server, remote_path, return_code)
-                else:
-                    log.info('同步文件成功: local_path=%s, server=%s, remote_path=%s, return_code=%d',
-                             local_path, server, remote_path, return_code)
-            if not running_processes:
-                break
+        while time.time() < deadline:
             time.sleep(1)
-        for server, process in running_processes:
-            log.error('同步文件超时: local_path=%s, server=%s, remote_path=%s, timeout=%d',
+            processes = running_process
+            running_process = []
+            for server, process in processes:
+                status = process.poll()
+                if status is None:
+                    running_process.append((server, process))
+                elif status != 0:
+                    log.error('同步失败: local_path=%s, server=%s, remote_path=%s, return_code=%d',
+                              local_path, server, remote_path, status)
+                else:
+                    log.info('同步成功: local_path=%s, server=%s, remote_path=%s, return_code=%d',
+                             local_path, server, remote_path, status)
+            if not running_process:
+                break
+        for server, process in running_process:
+            log.error('同步超时: local_path=%s, server=%s, remote_path=%s, timeout=%d',
                       local_path, server, remote_path, self._files_synchronizing_timeout)
-            process.kill()
-
-    def synchronize_directory(self, config):
-        log.debug('同步目录: %s', self._path)
-        processes = [(server,
-                      subprocess.Popen(['rsync', '-r', self._path, '%s:%s' % (server, config.remote_path)])
-                      if not ServerGroup.is_localhost(server) else
-                      subprocess.Popen('cp %s %s/' % (os.path.join(self._path, '*'), config.remote_path), shell=True))
-                     for server in ServerGroup.group(config.server_group)]
-        running_processes = processes
-        deadline = time.time() + self._directory_synchronizing_timeout
-        while 1:
-            if time.time() >= deadline:
-                break
-            processes = running_processes
-            running_processes = []
-            for server, process in processes:
-                return_code = process.poll()
-                if return_code is None:
-                    running_processes.append((server, process))
-                elif return_code != 0:
-                    log.error('同步文件夹失败: local_path=%s, server=%s, remote_path=%s, return_code=%d',
-                              self._path, server, config.remote_path, return_code)
-                else:
-                    log.info('同步文件夹成功: local_path=%s, server=%s, remote_path=%s, return_code=%d',
-                             self._path, server, config.remote_path, return_code)
-            if not running_processes:
-                break
-            time.sleep(1)
-        for server, process in running_processes:
-            log.error('同步文件夹超时: local_path=%s, server=%s, remote_path=%s, timeout=%d',
-                      self._path, server, config.remote_path, self._directory_synchronizing_timeout)
             process.kill()
 
 
@@ -155,7 +96,7 @@ class DirectoryMonitorApplication(jesgoo.application.Application):
     def main(self, args):
         super(DirectoryMonitorApplication, self).main(args)
         for server_group in self.config.directory_monitor.server_groups:
-            ServerGroup.group(server_group.name).extend(server_group.servers)
+            server_groups[server_group.name].extend(server_group.servers)
         for directory_monitor_config in self.config.directory_monitor.directories:
             directory_monitor = DirectoryMonitor(**directory_monitor_config.as_config_dict)
             path = os.path.abspath(directory_monitor_config.path)
